50983737d6c9b21d51ab37752b3dba6bdc674a36
fazlur zubayer <30690807+fzubayer@users.noreply.github.com>
2019-11-03 21:07:32 +0600

    Add files via upload
    purchase history, chat routing done

--- controllers/customer.js
+++ controllers/customer.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var userModel = require('./../models/user-model');
+var customerhistoryModel = require('./../models/customerhistory-model');
 var router = express.Router();
+var io = require('socket.io') ;
 
 router.get('*', function(request, response, next) {
 
@@ -14,7 +16,7 @@ router.get('*', function(request, response, next) {
 
 
 router.get('/', function(request, response) {
-    console.log("customer");
+    console.log("customer index");
     response.render('customer/index', { username: request.session.username, email: request.session.email });
 });
 // router.get('/details/:email', function(request, response) {
@@ -23,11 +25,18 @@ router.get('/', function(request, response) {
 //         response.render("admin/details", result);
 //     })
 // });
+router.get('/history/:email', function(request, response) {
+
+    customerhistoryModel.getByEmail(request.params.email, function(result) {
+        
+        response.render('customer/history', result);
+    });
+
+});
 router.get('/details/:email', function(request, response) {
 
     userModel.getByEmail(request.params.email, function(result) {
-        console.log("Deki ai porjonto ay ki na!!!!!!!!!!");
-        console.log(result);
+        
         response.render('customer/details', result);
     });
 
@@ -58,21 +67,52 @@ router.post('/edit/:email', function(request, response) {
         if (status) {
             response.redirect('../details/' + request.params.email);
         } else {
-            response.redirect('/admin/edit/' + request.params.email);
+            response.redirect('/customer/edit/' + request.params.email);
         }
     });
 
 });
+router.get('/chat/:email', function(request, response) {
+    console.log("chat get");
+
+    userModel.getByEmail(request.params.email, function(result) {
+        console.log('result');
+
+       
+        
+        response.render('customer/chat',result );
+       // io.sockets.on('connection', function(socket){
+              // console.log('Socket Connected...');
+              // console.log(result);
+             //  socket.on('email', function(data, callback);
+   
+
+     });
+    
+      }); 
+        
+
+router.get('/history/:email', function(request, response) {
+    console.log("history get");
+
+    userModel.getByEmail(request.params.email, function(result) {
+        console.log('result');
+
+       
+        
+        response.render('customer/history',result );
+       // io.sockets.on('connection', function(socket){
+              // console.log('Socket Connected...');
+              // console.log(result);
+             //  socket.on('email', function(data, callback);
+   
+
+     });
+    
+      }); 
+        
 
 
-router.get('/userList', function(request, response) {
-    console.log("userlist get");
-
-    userModel.getAll(function(result) {
-        console.log("userlist get all");
-        response.render('admin/userlist', { user: result });
-    });
-});
 
 
 module.exports = router;
--- controllers/manager.js
+++ controllers/manager.js
@@ -2,114 +2,11 @@ var express = require('express');
 var userModel = require('./../models/user-model');
 var router = express.Router();
 
-router.get('*', function(request, response, next) {
-
-    if (request.cookies['username'] != null) {
-        next();
-    } else {
-        response.redirect('/logout');
-    }
-
-});
-
-
 router.get('/', function(request, response) {
     console.log("manager");
-    response.render('manager/index', { username: request.session.username, email: request.session.email });
-});
-// router.get('/details/:email', function(request, response) {
-//     console.log("details");
-//     userModel.getByEmail(request.params.email, function(result) {
-//         response.render("admin/details", result);
-//     })
-// });
-router.get('/details/:email', function(request, response) {
-
-    userModel.getByEmail(request.params.email, function(result) {
-
-        console.log(result);
-        response.render('manager/details', result);
-    });
-
+    response.render('manager/index');
 });
 
-router.get('/edit/:email', function(request, response) {
-    console.log("edit get");
-
-    userModel.getByEmail(request.params.email, function(result) {
-        response.render('manager/edit', result);
-    });
-
-});
-
-router.post('/edit/:email', function(request, response) {
-    console.log("edit post");
-    var user = {
-        username: request.body.username,
-        password: request.body.password,
-        email: request.params.email,
-        phone: request.body.phone,
-        address: request.body.address,
-
-    };
-
-    userModel.update(user, function(status) {
-
-        if (status) {
-            response.redirect('../details/' + request.params.email);
-        } else {
-            response.redirect('/manager/edit/' + request.params.email);
-        }
-    });
-
-});
-
-
-router.get('/userList', function(request, response) {
-    console.log("userlist get");
-
-    userModel.getAll(function(result) {
-        console.log("userlist get all");
-        response.render('manager/userlist', { user: result });
-    });
-});
-
-router.post('/edit/:email', function(request, response) {
-    console.log("edit post");
-    var user = {
-        username: request.body.username,
-        password: request.body.password,
-        email: request.params.email,
-        phone: request.body.phone,
-        address: request.body.address,
-
-    };
-
-    userModel.update(user, function(status) {
-
-        if (status) {
-            response.redirect('../details/' + request.params.email);
-        } else {
-            response.redirect('/manager/edit/' + request.params.email);
-        }
-    });
-
-});
-
-
-router.get('/userList', function(request, response) {
-    console.log("userlist get");
-
-    userModel.getAll(function(result) {
-        console.log("userlist get all");
-        response.render('manager/userlist', { user: result });
-    });
-});
-
-router.get('/salary', function(request, response) {
-    console.log("manager");
-    response.render('manager/salary');
-});
 
 
 module.exports = router;
